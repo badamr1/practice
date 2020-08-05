bf46fdf187050768cb250d997c640e89be222def
RAMU <RAMU@RAMU-PC>
2020-08-05 12:45:31 +0530

    created test case 2

---
+++ src/test/java/GURU99/KICKOFF/testCase2.java
@@ -0,0 +1,31 @@
+package GURU99.KICKOFF;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class testCase2 {
+
+	@Test
+	public void f() {
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
